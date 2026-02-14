ecff65328548e35708574b58831e4f6b30832324
cs2sysfail <danilosantos280696@gmail.com>
2026-02-14 19:59:59 -0300

    Create matchmaking_dll.hpp

---
+++ matchmaking_dll.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+namespace cs2_dumper {
+    namespace offsets {
+namespace matchmaking_dll {
+            constexpr std::ptrdiff_t dwGameTypes = 0x1B8000;
+        }
