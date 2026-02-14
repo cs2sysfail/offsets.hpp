74d5a9e66b1e6f8363cbc2ccb88e637493978c2d
cs2sysfail <danilosantos280696@gmail.com>
2026-02-14 19:59:10 -0300

    Create imputsystem_dll.hpp

---
+++ imputsystem_dll.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+namespace cs2_dumper {
+    namespace offsets {
+namespace inputsystem_dll {
+            constexpr std::ptrdiff_t dwInputSystem = 0x45AD0;
+        }
