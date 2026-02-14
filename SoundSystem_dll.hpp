ac406258524f6f89177b08c24cbf1242df86e133
cs2sysfail <danilosantos280696@gmail.com>
2026-02-14 20:00:45 -0300

    Create SoundSystem_dll.hpp

---
+++ SoundSystem_dll.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+namespace cs2_dumper {
+    namespace offsets {
+namespace soundsystem_dll {
+            constexpr std::ptrdiff_t dwSoundSystem = 0x4F3490;
+            constexpr std::ptrdiff_t dwSoundSystem_engineViewData = 0x7C;
+        }
